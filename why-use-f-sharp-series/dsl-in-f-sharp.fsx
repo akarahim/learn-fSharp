48e51596015363563d9e5886e40c26590c879855
Rahim Khan <akarahim@hotmail.com>
2017-01-10 22:02:25 +0530

    completed dsl

--- why-use-f-sharp-series/dsl-in-f-sharp.fsx
+++ why-use-f-sharp-series/dsl-in-f-sharp.fsx
@@ -15,5 +15,49 @@ let getDate interval scale direction =
 let example1  = getDate 5 Days Ago
 let exmaple2 =  getDate 1 Hour Hence
 //Started Loving F#
+type FluentShape = {
+    label:string;
+    color:string;
+    onClick:FluentShape->FluentShape
+    }
+let defaultShape ={label="";color="";onClick=fun shape->shape}
+let click shape = 
+    shape.onClick shape
 
+let display shape =
+    printfn "My Label = %s and my color=%s" shape.label shape.color
+    shape
+let setLabel label shape = {shape with FluentShape.label = label}
+let setColor color shape = {shape with FluentShape.color = color}
+
+let appendClickAction action shape = {shape with FluentShape.onClick = shape.onClick>>action}
+let setRedBox = setColor "Red">> setLabel "box"
+let setBlueBox = setRedBox >> setColor "blue"
+let changeColorClick color =  appendClickAction (setColor color)
+let redBox = defaultShape|>setRedBox
+let blueBox = defaultShape|>setBlueBox
+redBox
+    |>display
+    |>changeColorClick "green"
+    |>click
+    |>display
+
+blueBox 
+    |>display
+    |>appendClickAction (setLabel "box2">>setColor "green")
+    |>click
+    |>display
+
+let rainbow=
+    ["red";"orange";"yellow";"green";"blue";"indigo";"violet"]
+let showRainbow =  
+    let setColorAndDisplay color = setColor color >> display
+    rainbow
+    |>List.map setColorAndDisplay
+    |>List.reduce (>>)
     
+defaultShape |>showRainbow
+
+
+
+
